65983af7aba0963334ad562bdecfb64ba61e717f
Mateojw <mjw557@nyu.edu>
2017-04-30 12:05:58 -0400

    working on new level
    added working door

---
+++ shift/Assets/Script/EnterDoorChangeLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterDoorChangeLevel : MonoBehaviour {
+	public string leveltoload; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void OnTriggerStay(Collider other){
+		print ("intrigger");
+		if (Input.GetKey(KeyCode.W)){ 
+			print ("pressed W");
+			Application.LoadLevel (leveltoload);
+		
+		}
+
+	}
+
+
+}
---
+++ shift/Assets/Script/changeScene.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class changeScene : MonoBehaviour {
+
+	public void changeToScene(int changeTheScene){
+		SceneManager.LoadScene (changeTheScene);
+	}
+
+}
