48dc5daf20e6177c1857fe7023549672cc00ed59
Mateojw <mjw557@nyu.edu>
2017-04-30 21:58:01 -0400

    added fade in
    actually alot harder than i thought

--- shift/Assets/Script/EnterDoorChangeLevel.cs
+++ shift/Assets/Script/EnterDoorChangeLevel.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class EnterDoorChangeLevel : MonoBehaviour {
 	public string leveltoload; 
+	private bool change;
 	// Use this for initialization
 	void Start () {
+		change = false;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+
+		if (change = true){
+		//float fadeTime = GameObject.Find ("Fade").GetComponent<Fading>().BeginFade (1);
+		//yield return new WaitForSeconds (fadeTime);
+		//Application.LoadLevel (Application.loadedLevel + 1);
+		}
 	}
 
 	private void OnTriggerStay(Collider other){
 		print ("intrigger");
 		if (Input.GetKey(KeyCode.W)){ 
 			print ("pressed W");
-			Application.LoadLevel (leveltoload);
+			change = true;
+		
+
+
+		//	Application.LoadLevel (leveltoload);
 		
 		}
 
---
+++ shift/Assets/Script/Fading.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// https://www.youtube.com/watch?v=0HwZQt94uHQ&t=41s
+public class Fading : MonoBehaviour {
+	public Texture2D FadeOutTexture;
+	public float fadeSpeed = 0.8f;
+	public int DrawDepth = -1000;
+	private float alpha = 1.0f;
+	private int fadeDir = -1;    //direction to fade
+
+	void OnGUI () {
+		alpha += fadeDir * fadeSpeed * Time.deltaTime;
+		// force clamp the number between 0 and 1 
+		alpha = Mathf.Clamp01(alpha);
+		GUI.color = new Color (GUI.color.r, GUI.color.g, GUI.color.b, alpha); 
+		GUI.depth = DrawDepth;
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), FadeOutTexture);
+
+	}
+
+	public float BeginFade (int direction) {
+		fadeDir = direction;
+		return(fadeSpeed);
+	
+	
+	}
+
+
+
+	void OnLevelWasLoaded() {
+		BeginFade (-1);
+
+
+
+	}
+
+
+
+
+}
