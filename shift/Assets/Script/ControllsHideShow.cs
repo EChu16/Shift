76b54dca56aa9a0df2f2a43f5567298a6294e9e4
Mateojw <mjw557@nyu.edu>
2017-05-02 11:09:55 -0400

    Working on intro controlls not done but close!

---
+++ shift/Assets/Script/ControllsHideShow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllsHideShow : MonoBehaviour {
+
+	public GameObject GO;
+	private bool change;
+	private SpriteRenderer sprite_renderer;
+	// Use this for initialization
+	void Start () {
+		sprite_renderer = GetComponent<SpriteRenderer> ();
+		change = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		if (change = true){
+			sprite_renderer.enabled = false;
+
+		}
+	}
+
+	private void OnTriggerStay(Collider other){
+		print ("intrigger");
+		if(Input.GetKey(KeyCode.Y))
+			{
+		change = true;
+
+		}
+
+
+		
+	
+	
+
+
+
+	}
+}
+
+
