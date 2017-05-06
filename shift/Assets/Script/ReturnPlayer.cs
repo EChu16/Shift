2132c4636185c3c459c5cd45bd348393df527e58
Mateojw <mjw557@nyu.edu>
2017-05-06 17:25:16 -0400

    fixes after IDMshowcase playtest

--- shift/Assets/Script/ReturnPlayer.cs
+++ shift/Assets/Script/ReturnPlayer.cs
@@ -1,20 +1,24 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class ReturnPlayer : MonoBehaviour {
-	
-	public Transform ReturnPoint;
-	
-	public void OnCollisionEnter(Collision other)
-	{
-		Debug.Log ("wererw");
-			other.transform.position = ReturnPoint.position;
-
-	}
-	public void OnTriggerEnter(Collider other)
-	{
-
-		other.transform.position = ReturnPoint.position;
+	public GameObject UI;
+	// Use this for initialization
+	void Start () {
 		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	private void OnTriggerStay(Collider other){
+		GameObject gameOverUI =  Instantiate (UI);
+		gameOverUI.GetComponent<Canvas>().worldCamera = Camera.main;
+		gameOverUI.GetComponent<Canvas> ().planeDistance = 10;
+	
+
+
 	}
 }
